8b8ad43e4f820fb13e0ecfe8de57c9b54a138939
EthsMC <ethankeating1313@gmail.com>
2025-03-01 06:14:54 -0500

    Added database config

--- ewastehero/lib/main.dart
+++ ewastehero/lib/main.dart
@@ -1,8 +1,26 @@
+import 'dart:convert';
+
 import 'package:ewastehero/screens/bin_page.dart';
 import 'package:ewastehero/screens/search_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart'; // Import your HomeScreen
+
+class Config {
+  static String databaseKey = '';
+
+  static Future<void> loadConfig() async {
+    // Load the config.json file from assets
+    String configString = await rootBundle.loadString('assets/config.json');
+
+    // Decode the JSON string into a map
+    Map<String, dynamic> configMap = jsonDecode(configString);
+
+    // Set the database key from the config file
+    databaseKey = configMap['databaseKey'];
+  }
+}
 
 void main() {
   runApp(EWasteHeroApp()); // Run the app
